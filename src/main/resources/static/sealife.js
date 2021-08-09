7b40688cb2bb2c3fbedb56de1a1f713cf8d4aff5
Lee-Dillon <lee.dillon1@outlook.com>
2021-08-09 16:54:55 +0100

    committing updates made since last friday, have now fully implemented CRUD functionality and adding some styling

--- src/main/resources/static/sealife.js
+++ src/main/resources/static/sealife.js
@@ -1,29 +1,51 @@
 let fishtable = document.querySelector('#fishtable');
 
 function getData(url, callback) {
-    let jsonData;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {(jsonData = data);
-         console.log(data)})
-      .then(() => callback(jsonData))
-      .catch((error) => console.log(error));
-  }
+  let jsonData;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      (jsonData = data);
+      console.log(data)
+    })
+    .then(() => callback(jsonData))
+    .catch((error) => console.log(error));
+}
+
 
 getData('http://localhost:9001/getAll', createTable);
 
 function createTable(tableData) {
-    var table = document.createElement('table');
-    var tableBody = document.createElement('tbody');
-  
-    console.log(tableData);
-    tableData.forEach(function(rowData) {
-      var row = document.createElement('tr');
-  
+  var table = document.createElement('table');
+  var tableBody = document.createElement('tbody');
+  var tableHead = document.createElement('th');
+
+  var heading_1 = document.createElement('th');
+  heading_1.innerHTML = "ID No.";
+  var heading_2 = document.createElement('th');
+  heading_2.innerHTML = "Name";
+  var heading_3 = document.createElement('th');
+  heading_3.innerHTML = "Colour";
+  var heading_4 = document.createElement('th');
+  heading_4.innerHTML = "Length";
+  var heading_5 = document.createElement('th');
+  heading_5.innerHTML = "Weight";
+
+  table.appendChild(heading_1);
+  table.appendChild(heading_2);
+  table.appendChild(heading_3);
+  table.appendChild(heading_4);
+  table.appendChild(heading_5);
+
+
+  console.log(tableData);
+  tableData.forEach(function (rowData) {
+    var row = document.createElement('tr');
+
     var id = document.createElement('td');
     id.appendChild(document.createTextNode(rowData.id));
     row.appendChild(id);
-      
+
     var name = document.createElement('td');
     name.appendChild(document.createTextNode(rowData.name));
     row.appendChild(name);
@@ -41,35 +63,83 @@ function createTable(tableData) {
     row.appendChild(weight);
 
     tableBody.appendChild(row);
-    });
-  
-    table.appendChild(tableBody);
-    fishtable.appendChild(table);
-  }
+  });
 
-document.querySelectorAll('#addfishform input');
-let datafromform = Array.from(document.querySelectorAll('#addfishform input'))
-.reduce((acc, input) => ({...acc,[input.id]: input.value }), {});
+  table.appendChild(tableBody);
+  fishtable.appendChild(table);
+}
 
-function postData(url, callback) {
-    fetch(url, {
+let form = document.querySelector('#addfishform');
+let addBtn = document.querySelector('#addfish');
+addBtn.addEventListener("click", function (event) {
+  postData(event)
+});
+
+function postData(event) {
+  event.preventDefault();
+  let fname = document.querySelector('#fishname').value;
+  let fcolour = document.querySelector('#fishcolour').value;
+  let flength = document.querySelector('#fishlength').value;
+  let fweight = document.querySelector('#fishweight').value;
+  fetch("http://localhost:9001/create", {
     method: 'post',
     headers: {
-        "Content-type": "application/json"
+      "Content-type": "application/json"
     },
     body: JSON.stringify(
-        {
-            datafromform
-        })
+      {
+        "name": fname,
+        "colour": fcolour,
+        "length": flength,
+        "weight": fweight
+      })
+  })
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+  window.location.reload();
 }
-.then(res => res.json())
-.then((data) => console.log(`Request succeeded with JSON response ${data}`))
-.catch((error) => console.log(`Request failed ${error}`)))
+
+let upBtn = document.querySelector('#updatefish');
+upBtn.addEventListener("click", function (event) {
+  updateData(event)
+});
+
+function updateData(event) {
+  event.preventDefault();
+  console.log('anything');
+  let id = document.querySelector('#fishid').value
+  let fname = document.querySelector('#fishname').value;
+  let fcolour = document.querySelector('#fishcolour').value;
+  let flength = document.querySelector('#fishlength').value;
+  let fweight = document.querySelector('#fishweight').value;
+  fetch(`http://localhost:9001/update/${id}`, {
+    method: 'put',
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(
+      {
+        "name": fname,
+        "colour": fcolour,
+        "length": flength,
+        "weight": fweight
+      })
+  })
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+  window.location.reload();
 }
 
-let button = document.querySelector("#addfish");
-button.addEventListener("submit", postData('http://localhost:9001/create', ));
-// postData('http://localhost:9001/create', postData);
+let delBtn = document.querySelector('#deleteBtn')
+delBtn.addEventListener("click", function (event) {
+  deleteData(event)
+});
 
-
-
+function deleteData(event) {
+  event.preventDefault();
+  let id = document.querySelector('#fishid').value
+  fetch(`http://localhost:9001/delete/${id}`, {
+    method: 'delete'
+  })
+  window.location.reload();
+}
